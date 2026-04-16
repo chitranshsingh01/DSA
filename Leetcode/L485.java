86175feb31fec440d1fa9fb5f579409cfc31aad7
Chitransh Singh <chitranshsingh02@gmail.com>
2026-04-16 14:07:49 +0530

    did some dsa

--- Leetcode/L485.java
+++ Leetcode/L485.java
@@ -1,11 +1,3 @@
-import java.util.Arrays;
-public class L485{
-    public static void main(String[] args) {
-        int[] arr={1,1,0,1,1,1};
-        System.out.println(findMaxConsecutiveOnes(arr));
-        
-    }
-    public static int findMaxConsecutiveOnes(int[] nums) {
-      
-    }
+public class L485 {
+    
 }
